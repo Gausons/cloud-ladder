53d5bcc2ad161efbabbf95ad56523267efa0fd7e
gc <gaocong@authing.cn>
2022-06-09 17:06:46 +0800

    init

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,67 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async getDepartmentList(config: any): Promise<any> {
+    const departments = [];
+    return departments;
+  }
+
+  async getGroupList(config: any): Promise<any> {
+    const groups = [];
+    return groups;
+  }
+
+  async getUserList(config: any): Promise<any> {
+    const users = [];
+    return users;
+  }
+
+  async getUser(index: any) {
+    return {};
+  }
+
+  async createUser(user: any) {
+    return user;
+  }
+
+  async updateUser(user: any) {
+    return user;
+  }
+
+  async deleteUser(user: any) {
+    return user;
+  }
+
+  async getDepartment(index: any) {
+    return {};
+  }
+
+  async createDepartment(department: any) {
+    return department;
+  }
+
+  async updateDepartment(department: any) {
+    return department;
+  }
+
+  async deleteDepartment(department: any) {
+    return department;
+  }
+
+  async getGroup(index: any) {
+    return {};
+  }
+
+  async createGroup(group: any) {
+    return group;
+  }
+
+  async updateGroup(group: any) {
+    return group;
+  }
+
+  async deleteGroup(group: any) {
+    return group;
+  }
 }
